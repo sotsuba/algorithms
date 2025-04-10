4c311d1d87310688f2612614212a785906465f35
sotsuba <tienson19273@gmail.com>
2025-04-10 23:48:30 +0700

    Add: 3 CSES date: 2025.04.10

--- Flight_Routes_Check.cpp
+++ Flight_Routes_Check.cpp
@@ -8,12 +8,11 @@ Link to problem: https://cses.fi/problemset/task/1668
 
 using std::cin, std::cout, std::vector, std::queue;
 
-void dfs(int s, vector<vector<int>> &adj, vector<bool> &visited, int &count) {
+void dfs(int s, vector<vector<int>> &adj, vector<bool> &visited) {
     if (visited[s]) return;
     visited[s] = true;
-    count++;
     for (auto v : adj[s]) 
-        dfs(v, adj, visited, count);
+        dfs(v, adj, visited);
 }
 
 int main() {
@@ -28,8 +27,8 @@ int main() {
 
     vector<bool> visited_1(n), visited_2(n);
     int count_1 = 0, count_2 = 0;
-    dfs(0, adj_1, visited_1, count_1);
-    dfs(0, adj_2, visited_2, count_2);
+    dfs(0, adj_1, visited_1);
+    dfs(0, adj_2, visited_2);
     // for (int i = 0; i < n; i++) cout << visited_1[i] << " \n"[i==n-1];
     // for (int i = 0; i < n; i++) cout << visited_2[i] << " \n"[i==n-1];
     for (int i = 0; i < n; i++) {
